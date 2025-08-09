9d6ac3c733adfb6d28bcfb9d1387b468455bd08e
Tiffany Chong <sciffany@gmail.com>
2025-08-09 12:23:32 +0800

    MOved pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,38 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/copy-of-home",
+        destination: "/products/all",
+        permanent: true, // sends a 301
+      },
+      {
+        source: "/copy-of-products",
+        destination: "/products/fixed-length",
+        permanent: true, // sends a 301
+      },
+      {
+        source: "/copy-of-products-2",
+        destination: "/products/variable-length",
+        permanent: true, // sends a 301
+      },
+      {
+        source: "/copy-of-products-3",
+        destination: "/products/barrier-seal",
+        permanent: true, // sends a 301
+      },
+      {
+        source: "/copy-of-products-4",
+        destination: "/products/special-purpose",
+        permanent: true, // sends a 301
+      },
+      {
+        source: "/copy-of-products-5",
+        destination: "/products/all",
+        permanent: true, // sends a 301
+      },
+    ];
+  },
+};
 export default nextConfig;
