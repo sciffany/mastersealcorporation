4e176becafc297f40718937aca976291ae7976c0
Tiffany Chong <sciffany@gmail.com>
2025-08-09 10:38:46 +0800

    SSG blog

--- src/app/post/[blog]/page.tsx
+++ src/app/post/[blog]/page.tsx
@@ -1,13 +1,26 @@
-"use client";
-
 import { blogPosts } from "@/app/lib/data";
 import ReactMarkdown from "react-markdown";
 import Navigation from "@/app/components/Navigation";
 import Image from "next/image";
 import BlogContent from "@/app/components/BlogContent";
+import { notFound } from "next/navigation";
+export const dynamic = "force-static";
+export const dynamicParams = false; // only build known paths
 
-export default function BlogPage({ params }: { params: { blog: string } }) {
+export async function generateStaticParams() {
+  return blogPosts.map((blog) => ({
+    blog: blog.slug,
+  }));
+}
+
+type BlogParams = { params: { blog: string } };
+
+export default function BlogPage({ params }: BlogParams) {
   const blog = blogPosts.find((blog) => blog.slug === params.blog);
+
+  if (!blog) {
+    return notFound();
+  }
 
   return (
     <div>
