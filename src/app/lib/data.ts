c54d4fd51410d223972990ce28b94c1569da82fb
Tiffany Chong <sciffany@gmail.com>
2025-09-30 21:20:20 +0800

    Everything

--- src/app/lib/data.ts
+++ src/app/lib/data.ts
@@ -1,13 +1,13 @@
 const seals: Seal[] = [
   {
-    slug: "fix-cable-seal",
-    name: "Cable Seal Fixed",
-    pdf: "/fixed-length/Cable_Seal_Fixed_Length_Brochure.pdf",
+    slug: "twinlock",
+    name: "Twinlock",
+    pdf: "/fixed-length/TWINlock_Brochure.pdf",
     category: "fixed-length",
     description:
-      "Cable seal which forms a loop when locked. for truck doors or container van",
-    moq: 5000,
-    image: "/fixed-length/Fix_Cable_Seal_2.avif",
+      "Plastic seal ideal for securing delivery truck & van doors from tampering.",
+    moq: "1,000",
+    image: "/fixed-length/TwinLock_Pastel_shades.JPG",
   },
   {
     slug: "hybrid",
@@ -15,8 +15,8 @@ const seals: Seal[] = [
     pdf: "/fixed-length/Hybrid_Brochure.pdf",
     category: "fixed-length",
     description:
-      "Hybrid seal is a metal seal that offers versatility of color-coding to identify origin and destination depots quickly.",
-    moq: "",
+      "Metal van seal that offers versatility of color-coding to identify origin and destination depots quickly.",
+    moq: "1,000",
     image: "/fixed-length/Hybrid_Metal_locked.jpg",
   },
   {
@@ -25,48 +25,19 @@ const seals: Seal[] = [
     pdf: "/fixed-length/Metal_T_Seal_Brochure.pdf",
     category: "fixed-length",
     description:
-      "Metal T Seal is a metal seal for delivery truck & van doors. Higher average breaking strength of 70 Kg.",
-    moq: "",
-    image: "/fixed-length/T_Seal_bundle.jpg",
+      "Metal van seal with hiigher break strength of 70 Kg.  Suitable for long bumpy trips",
+    moq: "1,000",
+    image: "/fixed-length/Metal_T_Seal_bundle.jpg",
   },
   {
-    slug: "twinlock",
-    name: "Twinlock",
-    pdf: "/fixed-length/TWINlock_Brochure.pdf",
+    slug: "fix-cable-seal",
+    name: "Cable Seal - Fix Length",
+    pdf: "/fixed-length/Cable_Seal_Fix_Length_Brochure.pdf",
     category: "fixed-length",
     description:
-      "Twinlock is a metal seal for securing delivery truck & van doors from tampering.",
-    moq: "",
-    image: "/fixed-length/TwinLock_Pastel_shades.JPG",
-  },
-  {
-    slug: "alphaseal",
-    name: "Alphaseal",
-    pdf: "/variable-length/Alphaseal.pdf",
-    category: "variable-length",
-    description:
-      "Alphaseal features transparent locking cap for visible tamper evidence on the locking jaw.",
-    moq: "",
-    image: "/variable-length/AlphaLock.avif",
-  },
-  {
-    slug: "dobolox",
-    name: "Dobolox",
-    pdf: "/variable-length/Dobolox_Brochure.pdf",
-    category: "variable-length",
-    description: "The double locking design enhances the security feature.",
-    moq: "",
-    image: "/variable-length/Dobolox.avif",
-  },
-  {
-    slug: "lightlock",
-    name: "Lightlock",
-    pdf: "/variable-length/LightLock_Brochure.pdf",
-    category: "variable-length",
-    description:
-      "Lightlock is for fire extinguisher, emergency cabinets, medical kits. (low break strength)",
-    moq: "",
-    image: "/variable-length/maseco.avif",
+      "Cable seal which forms a loop when locked. for truck doors or container van",
+    moq: "5,000",
+    image: "/fixed-length/Cable_Seal_Fix_Length.avif",
   },
   {
     slug: "mini-jawlock",
@@ -74,77 +45,157 @@ const seals: Seal[] = [
     pdf: "/variable-length/Mini_Jawlock_Brochure.pdf",
     category: "variable-length",
     description:
-      'Mini Jawlock is for sealing small holes. strap length in 150 mm (6"), 200 mm (8"), 300 mm (12"), 400 mm (16")',
-    moq: "",
-    image: "/variable-length/MJ_Tags.avif",
+      'For sealing small holes. Comes in 150 mm (6"), 200 mm (8"), 300 mm (12"), 400 mm (16")',
+    moq: "1,000",
+    image: "/variable-length/Mini-Jawlock.avif",
   },
   {
-    slug: "segur-lock",
-    name: "SegurLock",
-    pdf: "/variable-length/SegurLock_Brochure.pdf",
+    slug: "lightlock",
+    name: "Lightlock",
+    pdf: "/variable-length/LightLock_Brochure.pdf",
     category: "variable-length",
     description:
-      "SegurLock is for for sealing courier sacks, postal bags, cash bags, truck doors",
-    moq: "",
-    image: "/variable-length/segur.avif",
+      "Suitable for fire extinguisher, emergency devices, medical kits. ",
+    moq: "1,000",
+    image: "/variable-length/LightLock.jpg",
+  },
+  {
+    slug: "m-lock",
+    name: "M-Lock",
+    pdf: "/variable-length/M-Lock_Brochure.pdf",
+    category: "variable-length",
+    description:
+      "Offers economical sealing solution.  Single-piece moulding seal",
+    moq: "1,000",
+    image: "/variable-length/M-Lock.jpg",
+  },
+  {
+    slug: "nic-lock",
+    name: "Nic-Lock",
+    pdf: "/variable-length/Nic-Lock_Brochure.pdf",
+    category: "variable-length",
+    description:
+      "Offers economical sealing solution.  Single-piece moulding seal",
+    moq: "1,000",
+    image: "/variable-length/Nic-Lock.jpg",
   },
   {
     slug: "snc",
     name: "SNC",
     pdf: "/variable-length/SNC_Brochure.pdf",
     category: "variable-length",
-    description: 'Biodegredable (Bio-D) 200 mm (8") stocks available.',
-    moq: "",
+    description:
+      "Strap is smooth & flexible suitable for small holes. Biodegredable stocks available for 200 mmL",
+    moq: "1,000",
     image: "/variable-length/SNC.avif",
   },
   {
-    slug: "tera-lock",
-    name: "TeraLock",
+    slug: "dobolox",
+    name: "Dobolox",
+    pdf: "/variable-length/Dobolox_Brochure.pdf",
+    category: "variable-length",
+    description:
+      "Has 2 stainless locking mechansims that enhances the security feature.",
+    moq: "1,000",
+    image: "/variable-length/Dobolox.avif",
+  },
+  {
+    slug: "segurlock",
+    name: "SegurLock",
+    pdf: "/variable-length/SegurLock_Brochure.pdf",
+    category: "variable-length",
+    description: "Wide strap for sealing courier sacks, postal bags, cash bags",
+    moq: "10,000",
+    image: "/variable-length/Segurlock.avif",
+  },
+  {
+    slug: "alphaseal",
+    name: "Alphaseal",
+    pdf: "/variable-length/Alphaseal_Brochure.pdf",
+    category: "variable-length",
+    description:
+      "Features transparent locking cap & striped strap for more visible tamper evidence",
+    moq: "1,000",
+    image: "/variable-length/Alphaseal.jpg",
+  },
+  {
+    slug: "maxi-jawlock",
+    name: "MaXi Jawlock",
+    pdf: "/variable-length/MaXi_Jawlock_Brochure.pdf",
+    category: "variable-length",
+    description:
+      'Higher Break Strength than typical plastic seal.  Longest is 500vmm (20")',
+    moq: "1,000",
+    image: "/variable-length/MAXI_Jawlock.jpg",
+  },
+  {
+    slug: "novalock",
+    name: "NovaLock",
+    pdf: "/variable-length/Novaock_Brochure.pdf",
+    category: "variable-length",
+    description: "Features triple locking mechansim & transparent window. ",
+    moq: "10,000",
+    image: "/variable-length/Novalock.jpg",
+  },
+  {
+    slug: "teralock",
+    name: "TeraLock-X",
     pdf: "/variable-length/TeraLock_Type-X_Brochure.pdf",
     category: "variable-length",
     description:
-      "TeraLock Type-X features ruler scale on strap (to guide seal tightening level) and tail keeper slot (to tuck protruding tail)",
-    moq: "",
-    image: "/variable-length/teralock.avif",
-  },
-  {
-    slug: "nic-lock",
-    name: "Nic Lock",
-    pdf: "/variable-length/NicLock_Brochure.pdf",
-    category: "variable-length",
-    description: "",
-    moq: "",
-    image: "",
+      "TeraLock Type-X features ruler scale on strap (to guide seal tightening level) ",
+    moq: "10,000",
+    image: "/variable-length/Teralock.avif",
   },
   {
     slug: "alu-cable-seal",
     name: "Alulock Cable Seal",
-    pdf: "/barrier-seal/Cable_Seal_Brochure.pdf",
+    pdf: "/barrier-seal/Alulock_Cable_Seal_Brochure.pdf",
     category: "barrier-seal",
     description:
-      "For sealing container vans or high value items. Cable will fray when cut. Model D3.5 is ISO17712:2013 compliant. Classified as High Security Seal",
-    moq: 250,
-    image: "/barrier-seal/cableseal.avif",
+      "For container vans or high value items. Cable will fray when cut. The D3.5 is ISO17712:2013 compliant as High Security Seal",
+    moq: "250",
+    image: "/barrier-seal/AluLock.avif",
   },
   {
-    slug: "fix-cable-seal",
-    name: "Cable Seal Fixed",
-    pdf: "/barrier-seal/Cable_Seal_Fixed_Length_Brochure.pdf",
+    slug: "plaska",
+    name: "Cable Seal - Plaska",
+    pdf: "/barrier-seal/Cable_Seal_Plaska_Brochure.pdf",
     category: "barrier-seal",
     description:
-      "Cable seal which forms a loop when locked. for truck doors or container van",
-    moq: 5000,
-    image: "/barrier-seal/Fix_Cable_Seal_2.avif",
+      "A secure alternative to padlock. Can contain carcode and up to 13-digit serial numbers",
+    moq: "3,000",
+    image: "/barrier-seal/Cable_Seal_Plaska.jog",
+  },
+  {
+    slug: "cable-grip",
+    name: "Cable Grip Seal",
+    pdf: "/barrier-seal/Cable_Seal_Grip_Brochure.pdf",
+    category: "barrier-seal",
+    description:
+      "Cable seal with robust locking mechanism. Cable fray when cut.",
+    moq: "5,000",
+    image: "/barrier-seal/Cable_Seal_Grip.jpg",
   },
   {
     slug: "permaseal",
     name: "Permaseal",
-    pdf: "/barrier-seal/Permaseal_Brochure.pdf",
+    pdf: "/barrier-seal/Cable_Seal_Permaseal_Brochure.pdf",
     category: "barrier-seal",
     description:
       "High security cable seal with zinc locking house. Cable frays when cut and can't re-insert.",
-    moq: 5000,
-    image: "/barrier-seal/permaseal.avif",
+    moq: "5,000",
+    image: "/barrier-seal/Cable_Seal_Permaseal.avif",
+  },
+  {
+    slug: "fix-cable-seal",
+    name: "Cable Seal - Fix Length",
+    pdf: "/barrier-seal/Cable_Seal_Fix_Length_Brochure.pdf",
+    category: "barrier-seal",
+    description:
+      "Cable seal which forms a loop when locked. for truck doors or container van",
+    moq: "5,000",
+    image: "/barrier-seal/Cable_Seal_Fix_Length.avif",
   },
   {
     slug: "tav-bolt-bp",
@@ -153,8 +204,27 @@ const seals: Seal[] = [
     category: "barrier-seal",
     description:
       "Classified as High Security. Compliant to ISO 1771:2013. Ideal for freight containers",
-    moq: 200,
+    moq: "200",
     image: "/barrier-seal/TavBolt_BP.jpg",
+  },
+  {
+    slug: "tav-bolt-bp-ap",
+    name: "TavBolt - angled pin",
+    pdf: "",
+    category: "barrier-seal",
+    description:
+      "Classified as High Security. Compliant to ISO 1771:2013. Ideal for freight containers",
+    moq: "5,000",
+    image: "/barrier-seal/TavBolt_BP-angled_pin.jpg",
+  },
+  {
+    slug: "tav-bolt   ",
+    name: "TavBolt",
+    pdf: "/barrier-seal/TavBolt_Brochure.pdf",
+    category: "barrier-seal",
+    description: "Bolt seal with encapsulated pin. Complies with ISO17712:2013",
+    moq: "3,000",
+    image: "/barrier-seal/TavBolt.jpg",
   },
   {
     slug: "anchorlock",
@@ -162,8 +232,8 @@ const seals: Seal[] = [
     pdf: "/special-purpose/AnchorLock_Brochure.pdf",
     category: "special-purpose",
     description:
-      "AnchorLock is used together with sealing wire for sealing meters, tankers, and items which need longer length",
-    moq: 250,
+      "Used with sealing wire for sealing meters, tankers, and items which need longer length",
+    moq: "2,000",
     image: "/special-purpose/anchorlock.avif",
   },
   {
@@ -172,9 +242,86 @@ const seals: Seal[] = [
     pdf: "/special-purpose/SecuriTape_Brochure.pdf",
     category: "special-purpose",
     description:
-      "Warranty/void label for computer products, emergency kits, medical kits, security docs",
-    moq: "",
+      "Warranty / void label for computer products, emergency kits, medical kits, security docs",
+    moq: "1,000",
     image: "/special-purpose/securitape.avif",
+  },
+  {
+    slug: "cartlock",
+    name: "Cart Lock",
+    pdf: "",
+    category: "special-purpose",
+    description:
+      "Suitable for e-carts & airline trolleys. Easy to lock & remove manually",
+    moq: "10,000",
+    image: "/special-purpose/CartLock.jpg",
+  },
+  {
+    slug: "padlock",
+    name: "Padlock ",
+    pdf: "/special-purpose/PadLock_Brochure.pdf",
+    category: "special-purpose",
+    description: "A disposable alternative to padlock with keys",
+    moq: "200",
+    image: "/special-purpose/PadLock2.jpg",
+  },
+  {
+    slug: "twistseal",
+    name: "Twist Seal",
+    pdf: "/special-purpose/TWISTSeal_Brochure.pdf",
+    category: "special-purpose",
+    description: "For sealing meters, tankers",
+    moq: "1,000",
+    image: "/special-purpose/TwistSeal.jpg",
+  },
+  {
+    slug: "sealing wires",
+    name: "Sealing Wire",
+    pdf: "/special-purpose/Sealing_Wire_Brochure.pdf",
+    category: "special-purpose",
+    description: "Used together with wire seals",
+    moq: "1,000",
+    image: "/special-purpose/Sealing_Wire.jpg",
+  },
+  {
+    slug: "zip bags",
+    name: "Zip Bags & ZipLocks",
+    pdf: "",
+    category: "special-purpose",
+    description:
+      "Suitable for securing cheques, sensitive documents, evidences from tampering.",
+    moq: "300",
+    image: "/special-purpose/ZipPacks_ZipLocks.jpg",
+  },
+  {
+    slug: "card vault",
+    name: "Card Vault",
+    pdf: "",
+    category: "special-purpose",
+    description:
+      "Transparent case stores 8 decks of playing cards to be sealed with spiderlock",
+    moq: "",
+    image: "/special-purpose/Card_Vault.jpg",
+  },
+  {
+    slug: "cable tie",
+    name: "Cable Tie",
+    pdf: "",
+    category: "special-purpose",
+    description:
+      "For closing bins, sacks. Bundling wires.Tagging.. Not a security seal.",
+    moq: "Php3000",
+    image: "/special-purpose/Cable_Tie.jpg",
+  },
+  {
+    slug: "marker tie",
+    name: "Marker Tie",
+    pdf: "",
+    category: "special-purpose",
+    description:
+      "Provides a write-on surface to label wires, cables, tubes & hoses",
+    moq: "Php3000",
+    image: "/special-purpose/Marker_Tie.jpg",
   },
 ];
 
@@ -210,7 +357,7 @@ export type Seal = {
   category: string;
   image?: string;
   description?: string;
-  moq?: number | "";
+  moq?: number | string;
 };
 
 export type Category = {
